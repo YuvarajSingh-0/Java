87087f119c4ae0ef85815f7b41ee08a7db1e6ca3
YuvarajSingh-0 <83658757+YuvarajSingh-0@users.noreply.github.com>
2021-07-13 12:51:27 +0530

    Add files via upload

---
+++ AbstractExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ex4a {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String dec;
+        int n1,n2;
+        do{
+            System.out.println("Enter the Operation (add/sub): ");
+            String op=sc.next();
+            System.out.println("Enter the numbers: ");
+            n1=sc.nextInt();
+            n2=sc.nextInt();
+            if(op.equals("add")){
+                System.out.println("Addition: "+(n1+n2));
+            }
+            else
+                System.out.println("Subtraction: "+(n1-n2));
+                System.out.println("Do you want to continue? (y/n)");
+            dec=sc.next();
+            if((dec.toLowerCase()).equals("y")){
+                continue;
+            }
+            else
+                break;
+        }while(true);
+    }
+}
