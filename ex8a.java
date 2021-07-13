1bfdbaa8f61c4ef02c8c0362bce5c1df52676441
YuvarajSingh-0 <83658757+YuvarajSingh-0@users.noreply.github.com>
2021-07-13 11:48:40 +0530

    Add files via upload

---
+++ ex8a.java
@@ -0,0 +1,34 @@
+public class ex8a{
+    public static void main(String[] args) {
+        Thread1 p1=new Thread1();
+        Thread2 p2=new Thread2();
+        Thread3 p3=new Thread3();
+        p1.start();
+        p2.start();
+        p3.start();
+    }
+}
+class Thread1 extends Thread{
+    synchronized public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println("Good Morning");
+            try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+    }
+}
+class Thread2 extends Thread{
+    synchronized public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println("Hello");
+            try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+    }
+}
+class Thread3 extends Thread{
+    synchronized public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println("Welcome");
+            try {Thread.sleep(3000);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+    }
+}
